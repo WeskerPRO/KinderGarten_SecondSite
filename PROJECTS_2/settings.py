0bb3cb50e2b5ce8266c5955a66806b21aefbf2ef
WeskerPRO <yulchiboyshahzod@gmail.com>
2026-03-06 04:47:08 +0300

    updated

--- PROJECTS_2/settings.py
+++ PROJECTS_2/settings.py
@@ -1,18 +1,13 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv("config.env")
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''  # here you are to write your secret key
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = (os.getenv('DEBUG') == 'True') # SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = ['*'] # Allowed hosts, you can add allowed hosts
 
 # Application definition
 
