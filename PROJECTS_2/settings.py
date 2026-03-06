202eb203c93b6ce17147da009e021a37904305ec
WeskerPRO <yulchiboyshahzod@gmail.com>
2026-03-06 03:34:58 +0300

    updated

--- PROJECTS_2/settings.py
+++ PROJECTS_2/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gmsgksmkgslfmsfsmf&&slkfmkls*mfklsmflkms%lfmlksm$slkfmklsmfklsmfmsklfmlsmflks'
+SECRET_KEY = ''  # here you are to write your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
